bcf4977650ad10b4830095fe2108487f1e046e1b
wahaj323 <raowahaj323@gmail.com>
2025-05-18 16:09:32 +0500

    Refactor 01

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -97,7 +97,7 @@ const Form = () => {
           type="submit"
           className="w-full bg-blue-600 text-white py-2 rounded-lg hover:bg-blue-700 transition-colors"
         >
-          Submit
+          {userUpdating.status? "Update" : "Add"}
         </button>
       </form>
     </div>
